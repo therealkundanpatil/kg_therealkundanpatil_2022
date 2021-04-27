d0748838eed025606b7b35119f10ebd056753545
therealkundanpatil <kp28021996@gmail.com>
2021-04-27 12:35:58 -0400

    Read all the available data if any from the command line argument into array datastructure.
    Added check for handling empty command line argument.
    
    If data is not available empty string is printed on console.
    
    else declare and initialize hashmap that stores the photogenic information.
    
    Iterate over input array from index location 2 because data is available
    from that location onwards.
    
    Then checked if integer in string format is not a single digit number run another loop
    over that integer and, perform mapping for each digit. Once complete
    append it to the string output.
    
    If single digit integer appears in the input array, output returned by hashmap
    lookup is appended to the string output.
    
    Added logic for comma insertion till the second last element
    
    Finally the output that was stored in the string is printed on the
    console.

--- main.js
+++ main.js
@@ -1,1 +1,53 @@
-// adding main branch
+// Variable to store the output
+var answer = "";
+// Read all the available data from command line
+var data = process.argv;
+
+// This condition signals no command line arguments 
+if (data.length == 2){
+  console.log(answer);
+}
+// If command line arguments given, program enters here
+else{
+  //HashMap for mapping number to word
+  var mapNumberToWord = new Map([
+    ["0", "Zero"],
+    ["1", "One"],
+    ["2", "Two"],
+    ["3", "Three"],
+    ["4", "Four"],
+    ["5", "Five"],
+    ["6", "Six"],
+    ["7", "Seven"],
+    ["8", "Eight"],
+    ["9", "Nine"],
+  ]);
+
+
+  // Loop over the data
+  var i; 
+  for (i = 2; i < data.length; i++) {
+    // Checks for two digit and higher number strings
+    if (data[i].length > 1){
+      var elements = data[i].split("");
+      var combinedWords = "";
+      var j;
+      for ( j = 0; j < elements.length; j++ ){
+        combinedWords += mapNumberToWord.get(elements[j]);
+      }
+      answer += combinedWords;    
+    }
+    // Checks for single digit number string
+    else{
+      answer += mapNumberToWord.get(data[i]);
+    }
+
+    // Condition for inserting commas, till second last element append comma to output
+    if(i < data.length - 1){
+      answer += ",";
+    }  
+
+  }
+  // Print the output on console
+  console.log(answer);
+}
